cb95ed684471c9abd2b3e62402bad18d8c7a75fe
Sapithamol Peter <134997192+sapitha790@users.noreply.github.com>
2023-06-19 12:58:05 +0530

    Add files via upload

---
+++ pattern.py
@@ -0,0 +1,72 @@
+# # i(1,n+1)j(1,i+1)
+# n = int(input("enter number of rows:"))
+# for i in range(1, n+1):
+#     for j in range(1, i+1):
+#         print("*", end="")
+#     print()
+
+# i(n,0,-1)j(n,i-1,-1)
+# n = int(input("enter number of rows:"))
+# for i in range(n, 0, -1):
+#     for j in range(n, i-1, -1):
+#         print("*", end="")
+#     print()
+
+# n = int(input("enter number of rows:"))
+# for i in range(1, n+1):
+#     for j in range(1, i+1):
+#         print(j, end="")
+#     print()
+
+# n = int(input("enter number of rows:"))
+# for i in range(0, n+1):
+#     for j in range(0, i+1):
+#         print(j, end="")
+#     print()
+
+# n = int(input("enter number of rows:"))
+# for i in range(1, n+1):
+#     for j in range(1, i+1):
+#         print(i, end="")
+#     print()
+
+# n = int(input("enter number of rows:"))
+# for i in range(1, n+1):
+#     for j in range(i, n):
+#         print("*", end="")
+#     print()
+
+# n = int(input("enter number of rows:"))
+# for i in range(n, 0, -1):
+#     for j in range(n, i-1, -1):
+#         print(j, end="")
+#     print()
+
+# n = int(input("enter number of rows:"))
+# for i in range(1, (n-3)+1):
+#     for j in range(1, i+1):
+#         print("*", end="")
+#     print()
+# for k in range(1, (n-4)+1):
+#     for l in range(k, n-3):
+#         print("*", end="")
+#     print()
+
+#
+# n = int(input("enter number of rows:"))
+# for i in range(1, n+1):
+#     for j in range(1, n-i):
+#         print("&", end="")
+#     print()
+
+# n = int(input("enter number of rows:"))
+# for i in range(1,n+1):
+#     for j in range(i,n+1):
+#         print(j,end="")
+#     print()
+# n = int(input("enter number of rows:"))
+# for i in range(1, n+1):
+#     for j in range(1, i+1):
+#         print("*", end="")
+#     print()
+
